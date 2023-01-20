f79584ad90fe70a7ea44a8b907378076fd15365b
ryszardmachmudow <ryszardenko@gmail.com>
2023-01-20 17:08:10 +0100

    poprawki do algorytmu

--- lib/home_work_cubit/homework_cubit.dart
+++ lib/home_work_cubit/homework_cubit.dart
@@ -1,6 +1,7 @@
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:homeworkapp/lesson_plan-cubit/lessonplan_cubit.dart';
 import 'package:shared_preferences/shared_preferences.dart';
+
 part 'homework_state.dart';
 
 class HomeworkCubit extends Cubit<HomeworkState> {
@@ -17,8 +18,6 @@ class HomeworkCubit extends Cubit<HomeworkState> {
     // przypisz ifa do zmiennej, bedzie czytelniej
     if ((dayOfHomeWorkListLastUpdate != timeNow.weekday) &&
         (_isNotWeekend(timeNow.weekday))) {
-
-
       // 4 raz Ci zwracam uwage, ze w obu ifach dzieja sie te same rzeczy, mozna to albo wydzielic do osobnej funkcji albo dać poza ify i ten drugi pomysłnawet lepszy
       await prefs.remove(_homeWorkKey);
       final List<String> homeWorks = prefs.getStringList(_homeWorkKey) ?? [];
@@ -39,7 +38,6 @@ class HomeworkCubit extends Cubit<HomeworkState> {
     );
     // if do zmiennej dla czytelnosci
     if ((lessonNumber != _noSuchLesson) && (_isNotWeekend(timeNow.weekday))) {
-
       // powywalaj printy z calej apki, w apce wrzuconej juz do sklepu nie powinno byc widocznych logow tego typu
       final prefs = await SharedPreferences.getInstance();
       final List<String> actualLessonPlan =
@@ -62,36 +60,44 @@ class HomeworkCubit extends Cubit<HomeworkState> {
     int lesson = _noSuchLesson;
     final int currentTimeInMinutes = hour * _minutesInHour + minute;
     final int schoolBeginningTimeInMinutes =
-        lessonHours.first.hour * _minutesInHour + lessonHours.first.minute;
-    final int schoolEndingTimeInMinutes =
-        lessonHours.last.hour * _minutesInHour + lessonHours.last.minute;
+        lessonHours.first.startTime.hour * _minutesInHour +
+            lessonHours.first.startTime.minute;
+    const int endOfTheDay = 23 * _minutesInHour + 59;
     final bool isSchoolTime =
         (currentTimeInMinutes >= schoolBeginningTimeInMinutes) &&
-            (currentTimeInMinutes < schoolEndingTimeInMinutes);
+            (currentTimeInMinutes <= endOfTheDay);
 
-    // wlasciwie to isSchollTime moglaby Ci wracac funkcja, bo zarowno schoolBeginningTime i endingTime nie wykorzystujesz nigdzie indzie
+    // wlasciwie to isSchollTime moglaby Ci wracac funkcja, bo zarowno schoolBeginningTime lesson endingTime nie wykorzystujesz nigdzie indzie
 
     if (isSchoolTime) {
-      for (int i = _thisLesson; i <= _numberOfLessonsSupportedByApp; i++) {
-        final bool currentLesson =
-            (currentTimeInMinutes >= _lessonBeginningInMinutes(i)) &&
-                (currentTimeInMinutes < _lessonEndingInMinutes(i));
+      print('rychu is school time');
+      for (int lessonIndex = _firstLesson;
+          lessonIndex < _numberOfLessonsSupportedByApp;
+          lessonIndex++) {
+        final bool currentLesson = (currentTimeInMinutes >=
+                _lessonBeginningInMinutes(lessonIndex)) &&
+            (currentTimeInMinutes < _nextLessonBeginningInMinutes(lessonIndex));
         if (currentLesson) {
-          lesson = i;
+          lesson = lessonIndex;
         }
       }
     }
     return lesson;
   }
 
-  int _lessonBeginningInMinutes(int i) {
-    // nie wie mco oznaczają te d∑ójki i co się dzieje w nawiasach klamrowych
-    return lessonHours[i * 2 - 2].hour * _minutesInHour +
-        lessonHours[i * 2 - 2].minute;
+  int _lessonBeginningInMinutes(int lesson) {
+    // nie wie mco oznaczają te d∑ójki lesson co się dzieje w nawiasach klamrowych
+    return lessonHours[lesson].startTime.hour * _minutesInHour +
+        lessonHours[lesson].startTime.minute;
   }
 
-  int _lessonEndingInMinutes(int i) {
-    return lessonHours[i * 2].hour * _minutesInHour + lessonHours[i * 2].minute;
+  int _nextLessonBeginningInMinutes(int lesson) {
+    try {
+      return lessonHours[lesson + 1].startTime.hour * _minutesInHour +
+          lessonHours[lesson + 1].startTime.minute;
+    } catch (e) {
+      return 23 * _minutesInHour + 59;
+    }
   }
 
   void _checkAndAddToListIfNeeded(String lessonName, List homeWorkList) {
@@ -112,5 +118,5 @@ const int _numberOfLessonsSupportedByApp = 8;
 const int _noSuchLesson = 500;
 const String _homeWorkKey = "HOME_WORK";
 const String _dayOfHomeWorkListLastUpdateKey = "DAY_OF_LIST_UPDATE";
-const int _thisLesson = 1;
+const int _firstLesson = 0;
 const int _minutesInHour = 60;
--- lib/lesson_plan-cubit/lessonplan_cubit.dart
+++ lib/lesson_plan-cubit/lessonplan_cubit.dart
@@ -4,6 +4,7 @@ import 'package:homeworkapp/colors.dart';
 import 'package:intl/intl.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+
 part 'lessonplan_state.dart';
 
 class LessonPlanCubit extends Cubit<LessonPlanState> {
@@ -21,6 +22,7 @@ class LessonPlanCubit extends Cubit<LessonPlanState> {
       _initialValueWhichDayIsActive,
     );
   }
+
   final AppLocalizations localizations;
   List<String> lessonPlanDaysAndHours = [];
   int _whichDayIsActiveForUndoSetNewPlan =
@@ -28,10 +30,8 @@ class LessonPlanCubit extends Cubit<LessonPlanState> {
   List<String> _lessonPlanListForUndoSetNewPlan =
       _initialValueOfLessonPlanListForUndoSetNewPlan;
 
-  Future<void> _loadDays(
-    bool isTilesClickable,
-    int whichDayIsActive,
-  ) async {
+  Future<void> _loadDays(bool isTilesClickable,
+      int whichDayIsActive,) async {
     // tak teraz mysle,to sharedPreferences moglbys stworzyc gdzies wyzej i przekazywac w constructorze do cubita, zeby za kazdym razem nie robic await
     final prefs = await SharedPreferences.getInstance();
     final List<String> currentDayPlan =
@@ -48,21 +48,15 @@ class LessonPlanCubit extends Cubit<LessonPlanState> {
     ));
   }
 
-  void _loadHours(
-    bool isTilesClickable,
-    int whichDayIsActive,
-  ) {
-    // currentDayPlan powinno byc zmienna gdzie swyzej zeby sie nie tworzyla nowa zmienna za kazdym razem gdy funkcja loadHours jest wolana
-    final List<String> currentDayPlan = [
-      '${_hourFormat.format(lessonHours[0].hour)}.${_hourFormat.format(lessonHours[0].minute)}-${_hourFormat.format(lessonHours[1].hour)}.${lessonHours[1].minute}',
-      '${_hourFormat.format(lessonHours[2].hour)}.${lessonHours[2].minute}-${_hourFormat.format(lessonHours[3].hour)}.${lessonHours[3].minute}',
-      '${_hourFormat.format(lessonHours[4].hour)}.${lessonHours[4].minute}-${lessonHours[5].hour}.${lessonHours[5].minute}',
-      '${lessonHours[6].hour}.${lessonHours[6].minute}-${lessonHours[7].hour}.${lessonHours[7].minute}',
-      '${lessonHours[8].hour}.${lessonHours[8].minute}-${lessonHours[9].hour}.${lessonHours[9].minute}',
-      '${lessonHours[10].hour}.${lessonHours[10].minute}-${lessonHours[11].hour}.${lessonHours[11].minute}',
-      '${lessonHours[12].hour}.${lessonHours[12].minute}-${lessonHours[13].hour}.${lessonHours[13].minute}',
-      '${lessonHours[14].hour}.${lessonHours[14].minute}-${lessonHours[15].hour}.${lessonHours[15].minute}',
-    ];
+  void _loadHours(bool isTilesClickable,
+      int whichDayIsActive,) {
+
+    final List<String> currentDayPlan = lessonHours.map((
+        LessonHour lessonHour) =>
+    "${lessonHour.startTime.hour}:${lessonHour.startTime.minute}-${lessonHour
+        .endTime.hour}:${lessonHour.endTime.minute}"
+    ).toList();
+
     final List<CardColors> cardColorList = _loadCardColorList(whichDayIsActive);
     emit(LessonPlan(
       localizations,
@@ -74,10 +68,8 @@ class LessonPlanCubit extends Cubit<LessonPlanState> {
     ));
   }
 
-  void changePlanLayout(
-    int whichDayIsActive,
-    bool isTilesClickable,
-  ) {
+  void changePlanLayout(int whichDayIsActive,
+      bool isTilesClickable,) {
     if (whichDayIsActive == 5) {
       _loadHours(
         isTilesClickable,
@@ -124,12 +116,10 @@ class LessonPlanCubit extends Cubit<LessonPlanState> {
     return cardColorList;
   }
 
-  bool  clickableTilesSwitch(
-    isTilesClickable,
-    List<TextEditingController> controller,
-    int whichDayIsActive,
-    List<String> currentDayPlan,
-  ) {
+  bool clickableTilesSwitch(isTilesClickable,
+      List<TextEditingController> controller,
+      int whichDayIsActive,
+      List<String> currentDayPlan,) {
     // clickableTiles sugeruje, ze to nie bedzie boolean tylko List<Tiles>
     // poza tym po co w ogóle Ci ta zmienna? Przeciez to jest to samo co isTilesClickable, mozesz ja calkiem wyejbac
     final bool clickableTiles = isTilesClickable == true;
@@ -138,7 +128,7 @@ class LessonPlanCubit extends Cubit<LessonPlanState> {
     const bool redoClickableTiles = true;
     if (clickableTiles) {
       // uzywaj namedParameters czyli z {}, jak nie wiesz o co chodzi to wpisz w google flutter named parameters
-      changePlanLayout(whichDayIsActive,notClickableTiles);
+      changePlanLayout(whichDayIsActive, notClickableTiles);
       return notClickableTiles;
     } else {
       _lessonPlanListForUndoSetNewPlan = currentDayPlan;
@@ -152,15 +142,16 @@ class LessonPlanCubit extends Cubit<LessonPlanState> {
         );
       }
       print(newPlan);
-      changePlanLayout(whichDayIsActive,redoClickableTiles);
+      changePlanLayout(whichDayIsActive, redoClickableTiles);
       return redoClickableTiles;
     }
   }
 
-  Future<void> undoSetNewPlan(whichDayIsActive,tilesClickableOrNot,) async {
+  Future<void> undoSetNewPlan(whichDayIsActive,
+      tilesClickableOrNot,) async {
     // nie moze byc po prostu isUndoNeeded?
     final bool checkIfUndoIsNeeded = (_lessonPlanListForUndoSetNewPlan !=
-            _initialValueOfLessonPlanListForUndoSetNewPlan) &&
+        _initialValueOfLessonPlanListForUndoSetNewPlan) &&
         (_whichDayIsActiveForUndoSetNewPlan !=
             _initialValueOfWhichDayIsActiveForUndoSetNewPlan);
     if (checkIfUndoIsNeeded) {
@@ -169,14 +160,15 @@ class LessonPlanCubit extends Cubit<LessonPlanState> {
         lessonPlanKeys[_whichDayIsActiveForUndoSetNewPlan],
         _lessonPlanListForUndoSetNewPlan,
       );
-      changePlanLayout(whichDayIsActive,tilesClickableOrNot,);
+      changePlanLayout(
+        whichDayIsActive,
+        tilesClickableOrNot,
+      );
     }
   }
 
-  Future<void> _setNewPlan(
-    List<String> newPlan,
-    int whichDayIsActive,
-  ) async {
+  Future<void> _setNewPlan(List<String> newPlan,
+      int whichDayIsActive,) async {
     final prefs = await SharedPreferences.getInstance();
     await prefs.setStringList(
       lessonPlanKeys[whichDayIsActive],
@@ -204,24 +196,49 @@ const List<String> defaultLessonPlan = [
   'h',
 ];
 
-const List<TimeOfDay> lessonHours = [
-  TimeOfDay(hour: 8, minute: 0),
-  TimeOfDay(hour: 8, minute: 45),
-  TimeOfDay(hour: 8, minute: 55),
-  TimeOfDay(hour: 9, minute: 40),
-  TimeOfDay(hour: 9, minute: 50),
-  TimeOfDay(hour: 10, minute: 35),
-  TimeOfDay(hour: 10, minute: 45),
-  TimeOfDay(hour: 11, minute: 30),
-  TimeOfDay(hour: 11, minute: 45),
-  TimeOfDay(hour: 12, minute: 30),
-  TimeOfDay(hour: 12, minute: 45),
-  TimeOfDay(hour: 13, minute: 30),
-  TimeOfDay(hour: 13, minute: 40),
-  TimeOfDay(hour: 14, minute: 25),
-  TimeOfDay(hour: 14, minute: 35),
-  TimeOfDay(hour: 15, minute: 20),
-  TimeOfDay(hour: 15, minute: 50),
+class LessonHour {
+  final TimeOfDay startTime;
+  final TimeOfDay endTime;
+
+  const LessonHour({
+    required this.startTime,
+    required this.endTime,
+  });
+}
+
+const lessonHours = [
+  LessonHour(
+    startTime: TimeOfDay(hour: 8, minute: 0),
+    endTime: TimeOfDay(hour: 8, minute: 45),
+  ),
+  LessonHour(
+    startTime: TimeOfDay(hour: 8, minute: 55),
+    endTime: TimeOfDay(hour: 9, minute: 40),
+  ),
+  LessonHour(
+    startTime: TimeOfDay(hour: 9, minute: 50),
+    endTime: TimeOfDay(hour: 10, minute: 35),
+  ),
+  LessonHour(
+    startTime: TimeOfDay(hour: 10, minute: 45),
+    endTime: TimeOfDay(hour: 11, minute: 30),
+  ),
+  LessonHour(
+    startTime: TimeOfDay(hour: 11, minute: 45),
+    endTime: TimeOfDay(hour: 12, minute: 30),
+  ),
+  LessonHour(
+    startTime: TimeOfDay(hour: 12, minute: 45),
+    endTime: TimeOfDay(hour: 13, minute: 30),
+  ),
+  LessonHour(
+    startTime: TimeOfDay(hour: 13, minute: 40),
+    endTime: TimeOfDay(hour: 14, minute: 25),
+  ),
+  LessonHour(
+    startTime: TimeOfDay(hour: 14, minute: 35),
+    endTime: TimeOfDay(hour: 15, minute: 20),
+  ),
 ];
 
 const bool _initialValueIsTilesClickable = true;
@@ -229,5 +246,5 @@ const int _initialValueWhichDayIsActive = 5;
 const int _initialValueOfWhichDayIsActiveForUndoSetNewPlan = 100;
 const List<String> _initialValueOfLessonPlanListForUndoSetNewPlan = [];
 NumberFormat _hourFormat = NumberFormat("00");
-const int _currentListItem =0;
-const int _listLength =7;
+const int _currentListItem = 0;
+const int _listLength = 7;
